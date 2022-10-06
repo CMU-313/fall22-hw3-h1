97549267abd8fc2c3123fc988f123f410d9cca4c
Chloe Chan <chloec@andrew.cmu.edu>
2022-10-05 22:43:47 -0400

    updated controllers

--- docs-web/src/main/webapp/src/app/docs/controller/Statistics.js
+++ docs-web/src/main/webapp/src/app/docs/controller/Statistics.js
@@ -9,20 +9,20 @@ angular.module('docs').controller('Statistics', function($scope, Restangular) {
   $scope.score = { name: '', color: '#3a87ad' };
 
   // Retrieve statistics
-  $scope.GPAs = [];
-  $scope.GREs = [];
-  $scope.scores = [];
-  $scope.loadStatistics = function() {
-    Restangular.one('GPA/list').get().then(function(data) {
-      $scope.GPAs = data.GPAs;
-    });
-    Restangular.one('GRE/list').get().then(function(data) {
-        $scope.GREs = data.GREs;
-      });
-      Restangular.one('score/list').get().then(function(data) {
-        $scope.scores = data.scores;
-      });
-  };
-  $scope.loadStatistics();
+  $scope.GPAs = [1, 3, 5];
+  $scope.GREs = [2, 4, 6];
+  $scope.scores = [10, 11, 12];
+//   $scope.loadStatistics = function() {
+//     Restangular.one('GPA/list').get().then(function(data) {
+//       $scope.GPAs = data.GPAs;
+//     });
+//     Restangular.one('GRE/list').get().then(function(data) {
+//         $scope.GREs = data.GREs;
+//       });
+//       Restangular.one('score/list').get().then(function(data) {
+//         $scope.scores = data.scores;
+//       });
+//   };
+//   $scope.loadStatistics();
 
 });
