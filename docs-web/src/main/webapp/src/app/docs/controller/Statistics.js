9d1d435c63a531935eb2b8586267f3a081b55a15
Chloe Chan <chloec@andrew.cmu.edu>
2022-10-05 23:32:31 -0400

    added stuff to pull from backend but commented out and waitin for backend

--- docs-web/src/main/webapp/src/app/docs/controller/Statistics.js
+++ docs-web/src/main/webapp/src/app/docs/controller/Statistics.js
@@ -12,15 +12,10 @@ angular.module('docs').controller('Statistics', function($scope, Restangular) {
     {gpa: 3, gre: 30, score: 300}
   ];
 //   $scope.loadStatistics = function() {
-//     Restangular.one('GPA/list').get().then(function(data) {
-//       $scope.GPAs = data.GPAs;
+//     Restangular.one('statistic/list').get().then(function(data) {
+//       $scope.statistics = data.statistics
 //     });
-//     Restangular.one('GRE/list').get().then(function(data) {
-//         $scope.GREs = data.GREs;
-//       });
-//       Restangular.one('score/list').get().then(function(data) {
-//         $scope.scores = data.scores;
-//       });
+    
 //   };
 //   $scope.loadStatistics();
 
