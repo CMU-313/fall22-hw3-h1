99d207fb1d06883ee2eca3ea5f25c458d65f0b34
Chloe Chan <chloec@andrew.cmu.edu>
2022-10-05 23:01:44 -0400

    deleted useless variables in controller

--- docs-web/src/main/webapp/src/app/docs/controller/Statistics.js
+++ docs-web/src/main/webapp/src/app/docs/controller/Statistics.js
@@ -4,14 +4,8 @@
  * Statistics controller.
  */
 angular.module('docs').controller('Statistics', function($scope, Restangular) {
-  $scope.gpa = { name: '', color: '#3a87ad' };
-  $scope.gre = { name: '', color: '#3a87ad' };
-  $scope.score = { name: '', color: '#3a87ad' };
 
   // Retrieve statistics
-  $scope.GPAs = [1, 3, 5];
-  $scope.GREs = [2, 4, 6];
-  $scope.scores = [10, 11, 12];
   $scope.statistics = [
     {gpa: 1, gre: 10, score: 100},
     {gpa: 2, gre: 20, score: 200},
