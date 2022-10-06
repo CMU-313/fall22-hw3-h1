6a840fa6b677efaf04543da58a41fd2b78672719
Chloe Chan <chloec@andrew.cmu.edu>
2022-10-05 22:55:36 -0400

    aggregated stats?

--- docs-web/src/main/webapp/src/app/docs/controller/Statistics.js
+++ docs-web/src/main/webapp/src/app/docs/controller/Statistics.js
@@ -12,6 +12,11 @@ angular.module('docs').controller('Statistics', function($scope, Restangular) {
   $scope.GPAs = [1, 3, 5];
   $scope.GREs = [2, 4, 6];
   $scope.scores = [10, 11, 12];
+  $scope.statistics = [
+    {gpa: 1, gre: 10, score: 100},
+    {gpa: 2, gre: 20, score: 200},
+    {gpa: 3, gre: 30, score: 300}
+  ];
 //   $scope.loadStatistics = function() {
 //     Restangular.one('GPA/list').get().then(function(data) {
 //       $scope.GPAs = data.GPAs;
