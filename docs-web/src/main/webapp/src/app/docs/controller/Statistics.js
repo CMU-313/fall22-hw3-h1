b9116d8207480181c88044fdb9f637b601574162
Alexander <monkeyjerr@DESKTOP-U8NG0OG.localdomain>
2022-10-05 23:35:29 -0400

    small changes

--- docs-web/src/main/webapp/src/app/docs/controller/Statistics.js
+++ docs-web/src/main/webapp/src/app/docs/controller/Statistics.js
@@ -12,17 +12,17 @@ angular.module('docs').controller('Statistics', function($scope, Restangular) {
   $scope.GPAs = [1, 3, 5];
   $scope.GREs = [2, 4, 6];
   $scope.scores = [10, 11, 12];
-//   $scope.loadStatistics = function() {
-//     Restangular.one('GPA/list').get().then(function(data) {
-//       $scope.GPAs = data.GPAs;
-//     });
-//     Restangular.one('GRE/list').get().then(function(data) {
-//         $scope.GREs = data.GREs;
-//       });
-//       Restangular.one('score/list').get().then(function(data) {
-//         $scope.scores = data.scores;
-//       });
-//   };
-//   $scope.loadStatistics();
+  // $scope.loadStatistics = function() {
+  //   Restangular.one('GPA/list').get().then(function(data) {
+  //     $scope.GPAs = data.GPAs;
+  //   });
+  //   Restangular.one('GRE/list').get().then(function(data) {
+  //       $scope.GREs = data.GREs;
+  //     });
+  //     Restangular.one('score/list').get().then(function(data) {
+  //       $scope.scores = data.scores;
+  //     });
+  // };
+  // $scope.loadStatistics();
 
 });
