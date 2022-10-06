77013f2bdd0f31b75da528c35171e2cf57b25f66
Chloe Chan <chloec@andrew.cmu.edu>
2022-10-06 02:21:18 -0400

    updated stats controller to take from document

--- docs-web/src/main/webapp/src/app/docs/controller/Statistics.js
+++ docs-web/src/main/webapp/src/app/docs/controller/Statistics.js
@@ -11,12 +11,13 @@ angular.module('docs').controller('Statistics', function($scope, Restangular) {
     {gpa: 2, gre: 20, score: 200},
     {gpa: 3, gre: 30, score: 300}
   ];
-//   $scope.loadStatistics = function() {
-//     Restangular.one('statistic/list').get().then(function(data) {
-//       $scope.statistics = data.statistics
-//     });
+  $scope.loadStatistics = function() {
+    Restangular.one('document/list').get().then(function(data) {
+      $scope.statistics.push({gpa: data.GPA, gre: data.GRE, score: data.score});
+      console.log($scope.statistics);
+    });
     
-//   };
-//   $scope.loadStatistics();
+  };
+  $scope.loadStatistics();
 
 });
