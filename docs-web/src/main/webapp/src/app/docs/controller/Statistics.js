15b01f76f7af05b4339c4199d0f2aad4e213228e
Chloe Chan <chloec@andrew.cmu.edu>
2022-10-06 23:56:40 -0400

    improved prettines of stats page

--- docs-web/src/main/webapp/src/app/docs/controller/Statistics.js
+++ docs-web/src/main/webapp/src/app/docs/controller/Statistics.js
@@ -30,11 +30,37 @@ angular.module('docs').controller('Statistics', function($scope, Restangular) {
             Restangular.one('document/' + log.target).get().then(function (doc_data) {
               // console.log(doc_data);
               $scope.stats.push({gpa: doc_data.GPA, gre: doc_data.GRE, score: doc_data.score});
-              console.log($scope.stats);
             })
           });
         });
   };
   $scope.loadStatistics();
+
+  // aggregate statistics
+  // $scope.GPAcount = { '0 - 1.99': 0,'2.0 - 2.49': 0,'2.50 - 2.99': 0,'3.0 - 3.49': 0,'3.5 - 3.99': 0,'4.0 +': 0 };
+  $scope.GPAcount = [
+    { key: '0 - 1.99', count:0 },
+    { key: '2.0 - 2.49', count: 0 },
+    { key: '2.50 - 2.99', count: 0},
+    { key: '3.0 - 3.49', count: 0 },
+    { key: '3.5 - 3.99', count: 0 },
+    { key: '4.0 +', count: 0 }
+  ];
+  $scope.GREcount = [
+    { key: '260 - 280', count: 0 },
+    { key: '281 - 300', count: 0 },
+    { key: '301 - 320', count: 0 },
+    { key: '321 - 340', count: 0 },
+  ];
+
+  $scope.aggregateStatistics = function() { 
+    $scope.stats.forEach((stat, index) => {
+      $scope.GPAcount.count += 1;
+      $scope.GREcount.count += 1;
+    });
+    console.log($scope.GPAcount);
+    console.log($scope.GREcount);
+  };
+  $scope.aggregateStatistics();
 
 });
